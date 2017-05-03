aacc541ba8dd5cc4edc077129992544bb698dee6
Lewis C <redmechanic@users.noreply.github.com>
2017-05-03 12:52:36 +0100

    Update counting_sundays.py

--- Problem 19/counting_sundays.py
+++ Problem 19/counting_sundays.py
@@ -3,7 +3,7 @@ import calendar
 date = datetime(1901, 1, 1)
 count = 0
 while date < datetime(2000,12,31):
-    date = date + timedelta(days = 1)
+    date = date + timedelta(days = (1 if date.weekday() != 6 else 7))
     if date.weekday() == 6 and date.day == 1:
         count += 1
 print(count)
